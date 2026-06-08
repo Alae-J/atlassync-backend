5a785fb5e03cec030b5be02a8202ff78dff2505c
Alae-J <alaejahid8@gmail.com>
2026-06-08 13:47:37 +0100

    wire stripe properties from env

--- session-service/src/main/java/com/atlassync/session/SessionServiceApplication.java
+++ session-service/src/main/java/com/atlassync/session/SessionServiceApplication.java
@@ -2,8 +2,10 @@ package com.atlassync.session;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.atlassync.session")
 public class SessionServiceApplication {
 
     public static void main(String[] args) {
